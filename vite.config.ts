c9663fc32d5c6aadc3c6011963aca2f480815d84
Ravuthz <ravuthz@gmail.com>
2022-11-15 21:02:22 +0700

    Add antd with less

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePluginImp from 'vite-plugin-imp'
+import path from 'path'
+import fs from 'fs'
+import lessToJS from 'less-vars-to-js'
+
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, './src/assets/less/variables.less'), 'utf8')
+)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'antd',
+          style: (name) => `antd/lib/${name}/style/index.less`,
+        },
+      ]
+    })
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: themeVariables
+      }
+    }
+  },
 })
